1e2fa2c24ea924f334455abd0453baf2df3412b1
Kanarz <107653172+Kanarz@users.noreply.github.com>
2023-07-02 14:00:07 -0400

    Create V1.lua

---
+++ V1.lua
@@ -0,0 +1,294 @@
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+local TextLabel_4 = Instance.new("TextLabel")
+local Frame_2 = Instance.new("Frame")
+local UICorner_2 = Instance.new("UICorner")
+local TextButton = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local Frame_3 = Instance.new("Frame")
+local UICorner_4 = Instance.new("UICorner")
+local TextButton_2 = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local Frame_4 = Instance.new("Frame")
+local UICorner_6 = Instance.new("UICorner")
+local TextLabel_5 = Instance.new("TextLabel")
+local TextLabel_6 = Instance.new("TextLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.274206668, 0, 0.254807681, 0)
+Frame.Size = UDim2.new(0, 555, 0, 305)
+
+UICorner.CornerRadius = UDim.new(0.100000001, 8)
+UICorner.Parent = Frame
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.319127113, 0, 0.0346941948, 0)
+TextLabel.Size = UDim2.new(0, 200, 0, 28)
+TextLabel.Font = Enum.Font.Unknown
+TextLabel.Text = "KanarzHack"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(0.0740820542, 0, 0.126497477, 0)
+TextLabel_2.Size = UDim2.new(0, 136, 0, 18)
+TextLabel_2.Font = Enum.Font.Unknown
+TextLabel_2.Text = "Modules"
+TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextWrapped = true
+
+TextLabel_3.Parent = Frame
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.BorderSizePixel = 0
+TextLabel_3.Position = UDim2.new(0.376784742, 0, 0.126497477, 0)
+TextLabel_3.Size = UDim2.new(0, 136, 0, 18)
+TextLabel_3.Font = Enum.Font.Unknown
+TextLabel_3.Text = "Main"
+TextLabel_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.TextScaled = true
+TextLabel_3.TextSize = 14.000
+TextLabel_3.TextWrapped = true
+
+TextLabel_4.Parent = Frame
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.BackgroundTransparency = 1.000
+TextLabel_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.BorderSizePixel = 0
+TextLabel_4.Position = UDim2.new(0.679487467, 0, 0.126497477, 0)
+TextLabel_4.Size = UDim2.new(0, 136, 0, 18)
+TextLabel_4.Font = Enum.Font.Unknown
+TextLabel_4.Text = "Credits"
+TextLabel_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.TextScaled = true
+TextLabel_4.TextSize = 14.000
+TextLabel_4.TextWrapped = true
+
+Frame_2.Parent = Frame
+Frame_2.BackgroundColor3 = Color3.fromRGB(226, 226, 226)
+Frame_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame_2.BorderSizePixel = 0
+Frame_2.Position = UDim2.new(0.0612612627, 0, 0.222950816, 0)
+Frame_2.Size = UDim2.new(0, 150, 0, 218)
+
+UICorner_2.CornerRadius = UDim.new(0.100000001, 8)
+UICorner_2.Parent = Frame_2
+
+TextButton.Parent = Frame_2
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.BorderSizePixel = 0
+TextButton.Position = UDim2.new(0.0741031915, 0, 0.0596330389, 0)
+TextButton.Size = UDim2.new(0, 127, 0, 50)
+TextButton.Font = Enum.Font.Unknown
+TextButton.Text = "Auto Crate Open (Can't turn back off!)"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+
+UICorner_3.CornerRadius = UDim.new(0.100000001, 8)
+UICorner_3.Parent = TextButton
+
+Frame_3.Parent = Frame
+Frame_3.BackgroundColor3 = Color3.fromRGB(226, 226, 226)
+Frame_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame_3.BorderSizePixel = 0
+Frame_3.Position = UDim2.new(0.363963962, 0, 0.222950816, 0)
+Frame_3.Size = UDim2.new(0, 150, 0, 218)
+
+UICorner_4.CornerRadius = UDim.new(0.100000001, 8)
+UICorner_4.Parent = Frame_3
+
+TextButton_2.Parent = Frame_3
+TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.BorderSizePixel = 0
+TextButton_2.Position = UDim2.new(0.0741031915, 0, 0.0596330464, 0)
+TextButton_2.Size = UDim2.new(0, 127, 0, 50)
+TextButton_2.Font = Enum.Font.Unknown
+TextButton_2.Text = "Start Collecting Crates (Can't turn back off!)"
+TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.TextScaled = true
+TextButton_2.TextSize = 14.000
+TextButton_2.TextWrapped = true
+
+UICorner_5.CornerRadius = UDim.new(0.100000001, 8)
+UICorner_5.Parent = TextButton_2
+
+Frame_4.Parent = Frame
+Frame_4.BackgroundColor3 = Color3.fromRGB(226, 226, 226)
+Frame_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame_4.BorderSizePixel = 0
+Frame_4.Position = UDim2.new(0.666666627, 0, 0.222950816, 0)
+Frame_4.Size = UDim2.new(0, 150, 0, 218)
+
+UICorner_6.CornerRadius = UDim.new(0.100000001, 8)
+UICorner_6.Parent = Frame_4
+
+TextLabel_5.Parent = Frame_4
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.BackgroundTransparency = 1.000
+TextLabel_5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_5.BorderSizePixel = 0
+TextLabel_5.Position = UDim2.new(0.0461539701, 0, 0.211096674, 0)
+TextLabel_5.Size = UDim2.new(0, 136, 0, 25)
+TextLabel_5.Font = Enum.Font.Unknown
+TextLabel_5.Text = "Kanarz - GUI Design"
+TextLabel_5.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_5.TextScaled = true
+TextLabel_5.TextSize = 14.000
+TextLabel_5.TextWrapped = true
+
+TextLabel_6.Parent = Frame_4
+TextLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.BackgroundTransparency = 1.000
+TextLabel_6.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_6.BorderSizePixel = 0
+TextLabel_6.Position = UDim2.new(0.0461539701, 0, 0.0596330278, 0)
+TextLabel_6.Size = UDim2.new(0, 136, 0, 33)
+TextLabel_6.Font = Enum.Font.Unknown
+TextLabel_6.Text = "Slushpuppy - Scripts"
+TextLabel_6.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_6.TextScaled = true
+TextLabel_6.TextSize = 14.000
+TextLabel_6.TextWrapped = true
+
+-- Scripts:
+
+local function ABOE_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
+
+	
+	button = script.Parent
+	
+	script.Parent.MouseButton1Click:Connect(function()
+		
+	for a=1,99999999 do
+		irapeda4yearoldboy()
+		wait(5)
+	end
+	
+	end)
+	
+	function irapeda4yearoldboy()
+		game:GetService("ReplicatedStorage").MysteryBox:InvokeServer("Regular")
+		game:GetService("ReplicatedStorage").MysteryBox:InvokeServer("Unreal")
+		game:GetService("ReplicatedStorage").MysteryBox:InvokeServer("Birthday")
+		game:GetService("ReplicatedStorage").MysteryBox:InvokeServer("Inferno")
+	end
+	
+end
+coroutine.wrap(ABOE_fake_script)()
+local function PAFVZ_fake_script() -- TextButton_2.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_2)
+
+	
+	
+	button = script.Parent
+	
+	script.Parent.MouseButton1Click:Connect(function()
+	
+	local BoxFolder = game:GetService("Workspace").Boxes
+	
+	local HRP = game.Players.LocalPlayer.Character.HumanoidRootPart
+	
+	_G.CrateTP = true
+	
+	for i, v in pairs(BoxFolder:GetChildren()) do
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+		task.wait(0.25)
+	end
+	
+	BoxFolder.ChildAdded:Connect(function(child)
+		if _G.CrateTP then
+			pcall(function()
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = child.CFrame
+			end)
+		end
+	end)
+	
+	end)
+end
+coroutine.wrap(PAFVZ_fake_script)()
+local function MLNJPA_fake_script() -- Frame.Smooth GUI Dragging 
+	local script = Instance.new('LocalScript', Frame)
+
+	local UserInputService = game:GetService("UserInputService")
+	local runService = (game:GetService("RunService"));
+	
+	local gui = script.Parent
+	
+	local dragging
+	local dragInput
+	local dragStart
+	local startPos
+	
+	function Lerp(a, b, m)
+		return a + (b - a) * m
+	end;
+	
+	local lastMousePos
+	local lastGoalPos
+	local DRAG_SPEED = (8); -- // The speed of the UI darg.
+	function Update(dt)
+		if not (startPos) then return end;
+		if not (dragging) and (lastGoalPos) then
+			gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, lastGoalPos.X.Offset, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, lastGoalPos.Y.Offset, dt * DRAG_SPEED))
+			return 
+		end;
+	
+		local delta = (lastMousePos - UserInputService:GetMouseLocation())
+		local xGoal = (startPos.X.Offset - delta.X);
+		local yGoal = (startPos.Y.Offset - delta.Y);
+		lastGoalPos = UDim2.new(startPos.X.Scale, xGoal, startPos.Y.Scale, yGoal)
+		gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, xGoal, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, yGoal, dt * DRAG_SPEED))
+	end;
+	
+	gui.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = gui.Position
+			lastMousePos = UserInputService:GetMouseLocation()
+	
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+	
+	gui.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+	
+	runService.Heartbeat:Connect(Update)
+end
+coroutine.wrap(MLNJPA_fake_script)()
